28a75f87fd68fb120d66316ac2cc0fc34dc5f2d9
rypan <rongyupan@126.com>
2022-10-29 19:09:49 +0800

    初步运行成功

--- eval.sh
+++ eval.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-DTU_TESTING="/home/xyguo/dataset.ssd/dtu_mvs/processed/mvs_testing/dtu/"
-CKPT_FILE="./checkpoints/d192/model_000014.ckpt"
-python eval.py --dataset=dtu_yao_eval --batch_size=1 --testpath=$DTU_TESTING --testlist lists/dtu/test.txt --loadckpt $CKPT_FILE $@
+DTU_TESTING="./data/dtu"
+CKPT_FILE="./checkpoints/model_000014.ckpt"
+python eval.py --dataset=dtu_yao_eval --batch_size=2 --testpath=$DTU_TESTING --testlist lists/dtu/test.txt --loadckpt $CKPT_FILE $@
